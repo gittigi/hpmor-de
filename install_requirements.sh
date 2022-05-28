6b72f8756827366b6877eb0adbde9f843211b421
Torben <59419684+entorb@users.noreply.github.com>
2022-05-28 15:35:41 +0200

    Update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 # pdf
-sudo apt install texlive-xetex latexmk
+sudo apt install texlive-xetex latexmk texlive-extra-utils
+
 
 # ebook
 # TODO
 # sudo apt install calibre pandoc
 
 # doc
-sudo apt install pandoc
+# texlive-extra-utils needed for latexpand
+sudo apt install pandoc texlive-extra-utils
