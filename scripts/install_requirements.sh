8e1b8ad661a3dbfc7b7e5be0e0e8462284529520
Torben <59419684+entorb@users.noreply.github.com>
2022-06-26 11:23:46 +0200

    Update install_requirements.sh

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+sudo apt update
 sudo apt install texlive-xetex latexmk texlive-extra-utils pandoc calibre imagemagick ghostscript
 # LaTeX: texlive-xetex latexmk
 # ebook generation: pandoc calibre
