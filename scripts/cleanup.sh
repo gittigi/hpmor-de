3a0d2933df62c6b7d6a03a3737402ad91747d97f
Torben <59419684+entorb@users.noreply.github.com>
2023-01-06 21:05:50 +0100

    fix script_dir

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # ensure we are in the hpmor root dir
-script_dir=$(cd `dirname $0` && pwd)
+script_dir=$(cd $(dirname $0) && pwd)
 cd $script_dir/..
 
 latexmk -C
 rm -rf chapters/*.aux
 rm -rf chapters/*-autofix.tex
-rm -rf ebook/tmp
+rm -rf tmp/
